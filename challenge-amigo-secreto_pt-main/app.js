da6a3c2121eba68f98be5dd1f0dafd38c9aad18b
Arielli Nakata <ariellinakata@gmail.com>
2025-03-12 23:37:47 -0300

    contemplando função adicionarAmigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-//teste
+
+let amigos = [];
+
+function adicionarAmigo() {
+  //let textoBotaoAmigo = document.getElementById('amigo').value
+    let inputBotaoAmigo = document.querySelector('.input-name')
+    let textoBotaoAmigo = inputBotaoAmigo.value.trim()
+    
+    if(textoBotaoAmigo){
+        amigos.push(textoBotaoAmigo)
+        inputBotaoAmigo.value = ''
+
+    }else{
+        alert('Por favor, insira um nome.')
+    }
+        
+    console.log(amigos);
+}
