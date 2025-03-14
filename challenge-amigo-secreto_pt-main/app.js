7dee41ff22c014b0d59be2e12e01c3c3be68db3a
Arielli Nakata <ariellinakata@gmail.com>
2025-03-13 23:08:27 -0300

    contemplando funçãoAtualizarListaAmigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -4,12 +4,13 @@ let amigos = [];
 
 function adicionarAmigo() {
   //let textoBotaoAmigo = document.getElementById('amigo').value
-    let inputBotaoAmigo = document.querySelector('.input-name')
-    let textoBotaoAmigo = inputBotaoAmigo.value.trim()
+    let inputBotaoAmigo = document.querySelector('.input-name');
+    let textoBotaoAmigo = inputBotaoAmigo.value.trim();
     
     if(textoBotaoAmigo){
-        amigos.push(textoBotaoAmigo)
-        inputBotaoAmigo.value = ''
+        amigos.push(textoBotaoAmigo);
+        inputBotaoAmigo.value = '';
+        atualizarListaAmigo();
 
     }else{
         alert('Por favor, insira um nome.')
@@ -17,3 +18,34 @@ function adicionarAmigo() {
         
     console.log(amigos);
 }
+
+// function atualizarListaAmigo() {
+//     let listaAmigos = document.querySelector('#listaAmigos');
+//     listaAmigos.innerHTML = '';
+//     let cont = 0;
+//     while(cont<amigos.length){
+//         let li = document.createElement('li');
+//         li.textContent = amigos[cont]
+//         listaAmigos.appendChild(li);
+//         cont++;
+
+//     }
+
+// }
+
+function atualizarListaAmigo() {
+    let ulTagListaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = '';
+    amigos.forEach(amigo =>{
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        ulTagListaAmigos.appendChild(li);
+
+    })
+
+    let li = document.createElement('li');
+        li.textContent = amigos[cont]
+        listaAmigos.appendChild(li);
+        cont++;
+
+    }
