c45c6b6010da2ed3ce52d67debc505b480e8f712
Arielli Nakata <ariellinakata@gmail.com>
2025-03-18 22:03:37 -0300

    Removendo o console.log

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let amigos = [];
 
 function adicionarAmigo() {
@@ -14,8 +12,7 @@ function adicionarAmigo() {
     } else {
         alert('Por favor, insira um nome.')
     }
-
-    console.log(amigos);
+    
 }
 
 // function atualizarListaAmigo() {
